76e1c4f7968a28deb8663e832dc3d8730c19bb10
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-04-15 15:19:52 -0400

    Eric Fournier 20200415

--- Covid19/MetadataSimulator.py
+++ Covid19/MetadataSimulator.py
@@ -4,20 +4,18 @@ Eric Fournier 20200415
 
 import os
 from SequenceSimulator import CreateFastaCovid
-
-fasta_in_file =  os.path.join(os.path.dirname(os.getcwd()),"data/gisaid_covid19_usa_1759.fasta")
-fasta_in_file_test =  os.path.join(os.path.dirname(os.getcwd()),"data/three_seq.fasta")
-fasta_out_10seq_homogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_10seq_homogeneous.fasta")
-fasta_out_200seq_heterogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_200seq_heterogeneous.fasta")
-fasta_out_10seq_heterogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_10seq_heterogeneous.fasta")
+import random
 
 location_file = os.path.join(os.path.dirname(os.getcwd()),"config/ordering.tsv")
 
-#CreateFastaCoivid(fasta_in_file = fasta_in_file, fasta_out_file=fasta_out_10seq_heterogeneous, nb_seq=10, homogeneous=False)
-
-
 class MetadataBuilder(object):
-    def __init__(self):
+    def __init__(self, number_of_isolate, is_homogeneous, fasta_in, fasta_out):
+        self.fasta_in = fasta_in
+        self.fasta_out = fasta_out
+
+        self.location_file = os.path.join(os.path.dirname(os.getcwd()),"config/ordering.tsv")
+        self.number_of_isolate = number_of_isolate
+        self.is_homogeneous = is_homogeneous
 
         self.header = {'Strain' : 'strain', 'Virus' : 'virus', 'Rta' : 'rta', 'CountryExposure' : 'coutry_exposure',
         'ProvinceExposure' : 'province_exposure', 'Rta_exposure' : 'rta_exposure', 'Segment' : 'segment', 'Length' : 'length',
@@ -30,8 +28,19 @@ class MetadataBuilder(object):
         self.province_list = []
         self.country_list = []
 
+
+        self.random_rta_list = []
+        self.random_province_list = []
+        self.random_country_list = []
+
+        #CreateFastaCovid(fasta_in_file = self.fasta_in, fasta_out_file = self.fasta_out, nb_seq = self.number_of_isolate, homogeneous = is_homogeneous)
+        self.BuildLocationList()
+#       self.CreateRandomRtaList()
+        self.CreateRandomProvinceList()
+        self.CreateRandomCountryList()
+
     def BuildLocationList(self): 
-        with open(location_file) as readf:
+        with open(self.location_file) as readf:
             for line in readf:
                 info = line.split('\t')
                 self.location_dict[info[0]].add(info[1].strip('\n'))
@@ -40,6 +49,31 @@ class MetadataBuilder(object):
         self.province_list = list(self.location_dict['province'])
         self.country_list = list(self.location_dict['country'])
 
-mbuilder = MetadataBuilder()
-mbuilder.BuildLocationList()
+    def CreateRandomRtaList(self):
+        for i in range(0,self.number_of_isolate):
+            rnd_index = random.randint(0,len(self.rta_list - 1))
+            self.random_rta_list.append(self.rta_list[rnd_index]) 
 
+    def CreateRandomProvinceList(self):
+        for i in range(0,self.number_of_isolate):
+            rnd_index = random.randint(0,len(self.province_list) - 1)
+            self.random_province_list.append(self.province_list[rnd_index]) 
+
+    def CreateRandomCountryList(self):
+        for i in range(0,self.number_of_isolate):
+            rnd_index = random.randint(0,len(self.country_list) - 1)
+            self.random_country_list.append(self.country_list[rnd_index]) 
+
+
+fasta_in_file =  os.path.join(os.path.dirname(os.getcwd()),"data/gisaid_covid19_usa_1759.fasta")
+fasta_in_file_test =  os.path.join(os.path.dirname(os.getcwd()),"data/three_seq.fasta")
+fasta_out_10seq_homogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_10seq_homogeneous.fasta")
+fasta_out_200seq_heterogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_200seq_heterogeneous.fasta")
+fasta_out_10seq_heterogeneous =  os.path.join(os.path.dirname(os.getcwd()),"data/covid19_10seq_heterogeneous.fasta")
+number_of_isolate = 10
+is_homogeneous_seq = False
+
+mbuilder = MetadataBuilder(number_of_isolate, is_homogeneous_seq, fasta_in_file, fasta_out_10seq_homogeneous)
+#print(mbuilder.random_rta_list)
+#print(mbuilder.random_province_list)
+print(mbuilder.random_country_list)
