5864b175a6673eeef14524c7685f4bec60a2914f
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-04-22 15:35:02 -0400

    Eric Fournier 2020-04-22

--- Covid19/ChangeSeqIdFromMetadata.py
+++ Covid19/ChangeSeqIdFromMetadata.py
@@ -11,6 +11,8 @@ import logging
 
 basedir = "/data/PROJETS/COVID_19/TestMetadata"
 
+ref_ids = ["Wuhan-Hu-1/2019","Wuhan/WH01/2019"]
+
 metadata = os.path.join(basedir,"metadata.tsv")
 metadata_handle = open(metadata)
 metadata_handle.readline()
@@ -19,27 +21,26 @@ fake_seq_file = os.path.join(basedir,"sequences_fakeid.fasta")
 new_seq_file = os.path.join(basedir,"sequences.fasta")
 
 new_rec_list = []
+new_rec_list_id =  []
 
 logging.basicConfig(level = logging.DEBUG)
 
 metadata_id = []
 
 for line in metadata_handle:
-    #print(line)
     id = line.split("\t")[0]
     metadata_id.append(id)
-
-#print(str(metadata_id))
 
 metadata_handle.close()
 
 inc = 0
 
 for rec in SeqIO.parse(fake_seq_file,'fasta'):
-    #print(rec.id)
-    new_rec = SeqRecord(seq = rec.seq, id = metadata_id[inc], description = "" )
-    inc += 1
-    new_rec_list.append(new_rec)
+
+    if(inc < len(metadata_id)):
+        new_rec = SeqRecord(seq = rec.seq, id = metadata_id[inc], description = "" )
+        new_rec_list.append(new_rec)
+        inc += 1
 
 SeqIO.write(new_rec_list,new_seq_file,"fasta")
 
