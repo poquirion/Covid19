1fde7df1e74ef1c18671ca3ec2a2c272b747fd6b
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-04-26 16:42:50 -0400

    Eric Fournier 20200426

--- Covid19/PrepareMetadata.py
+++ Covid19/PrepareMetadata.py
@@ -35,7 +35,8 @@ for rec in SeqIO.parse(lspq_sequences,'fasta'):
 subset_lspq = df_lspq[df_lspq['NO_LSPQ'].isin(rec_id_list)]
 subset_gisaid = df_gisaid[df_gisaid['strain'].isin(rec_id_list)]
 
-subset_lspq_subcol = subset_lspq[['NO_LSPQ','DATE_PRELEV','SEX','AGE','RSS_PATIENT','POSTAL_CODE','VOYAGE_PAYS_1']]
+#subset_lspq_subcol = subset_lspq[['NO_LSPQ','DATE_PRELEV','SEX','AGE','RSS_PATIENT','POSTAL_CODE','VOYAGE_PAYS_1']] this create a view https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+subset_lspq_subcol = subset_lspq.loc[:,('NO_LSPQ','DATE_PRELEV','SEX','AGE','RSS_PATIENT','POSTAL_CODE','VOYAGE_PAYS_1')] # this create a copy
 subset_lspq_subcol.columns = ['strain','date','sex','age','rss','rta','voyage']
 subset_lspq_subcol.insert(loc=1,column='virus',value='ncov',allow_duplicates=True)
 subset_lspq_subcol.insert(loc=5,column='country',value='Canada',allow_duplicates=True)
@@ -54,7 +55,22 @@ subset_lspq_subcol.loc[subset_lspq_subcol.voyage != 'AUCUN_VOYAGE','rss_exposure
 subset_lspq_subcol.loc[subset_lspq_subcol.voyage != 'AUCUN_VOYAGE','rta_exposure'] = subset_lspq_subcol.voyage
 
 pd.set_option('display.max_columns', 20)
+del subset_lspq_subcol['voyage']
 #print(subset_lspq_subcol)
 
-subset_gisaid_subcol = subset_gisaid[['strain','virus','date','sex','age','country','division']]
-print(subset_gisaid_subcol)
+subset_gisaid_subcol = subset_gisaid.loc[:,('strain','virus','date','sex','age','country','division','country_exposure','division_exposure')]
+subset_gisaid_subcol.insert(loc=7,column='rss',value='',allow_duplicates=True)
+subset_gisaid_subcol.insert(loc=8,column='rta',value='',allow_duplicates=True)
+subset_gisaid_subcol.insert(loc=11,column='rss_exposure',value='',allow_duplicates=True)
+subset_gisaid_subcol.insert(loc=12,column='rta_exposure',value='',allow_duplicates=True)
+subset_gisaid_subcol.loc[:,'rss'] = subset_gisaid_subcol.country
+subset_gisaid_subcol.loc[:,'rta'] = subset_gisaid_subcol.country
+subset_gisaid_subcol.loc[:,'rss_exposure'] = subset_gisaid_subcol.country_exposure
+subset_gisaid_subcol.loc[:,'rta_exposure'] = subset_gisaid_subcol.country_exposure
+#print(subset_gisaid_subcol)
+
+#print(subset_lspq_subcol.columns)
+#print(subset_gisaid_subcol.columns)
+
+subset_lspq_subcol.to_csv(out_metadata,sep="\t",index=False)
+subset_gisaid_subcol.to_csv(out_metadata,sep="\t",index=False,mode='a',header=False)
