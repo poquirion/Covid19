e592d33af34822f7704afc0f812cf1c17cd884e5
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-04-26 22:38:51 -0400

    Eric Fournier 20200426

--- Covid19/PrepareMetadata.py
+++ Covid19/PrepareMetadata.py
@@ -1,3 +1,8 @@
+"""
+Eric Fournier 2020-04-26
+
+"""
+
 import os
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
@@ -9,14 +14,14 @@ base_dir = "/data/Applications/GitScript/Covid19/NextStrainFiles/"
 gisaid_metadata = os.path.join(base_dir,"data/original/data/metadata.tsv") 
 lspq_sgil_extract = os.path.join(base_dir,"data/sgil_extract.tsv")
 
+gisaid_ref_sequences = os.path.join(base_dir,"data/gisaid/gisaid_wuhan_ref_20200425.fasta")
 gisaid_sequences = os.path.join(base_dir,"data/gisaid/randomseq.fasta")
 lspq_sequences = os.path.join(base_dir,"data/lspq/sequences.fasta")
 
 df_lspq = pd.read_csv(lspq_sgil_extract,delimiter="\t",index_col=False)
 df_gisaid = pd.read_csv(gisaid_metadata,delimiter="\t",index_col=False)
 
-test =df_lspq[df_lspq['NO_LSPQ'].isin(['L00251524','Lkkk00251535'])]
-
+rec_list = []
 rec_id_list = []
 
 out_metadata = os.path.join(base_dir,"data/metadata.tsv")
@@ -25,12 +30,22 @@ out_sequences = os.path.join(base_dir,"data/sequences.fasta")
 for rec in SeqIO.parse(gisaid_sequences,'fasta'):
     seqid = re.search(r'^\S+\/(\S+\/\S+\/\d{4})\|\S+',rec.id).group(1)
     rec_id_list.append(seqid)
+    rec_list.append(rec)
+
+
+for rec in SeqIO.parse(gisaid_ref_sequences,'fasta'):
+    try:
+        seqid = re.search(r'^\S+\/(\S+\/\S+\/\d{4})\|\S+',rec.id).group(1)
+    except:
+        seqid = re.search(r'^\S+\/(\S+\/\d{4})\|\S+',rec.id).group(1)
+    rec_id_list.append(seqid)
+    rec_list.append(rec)
 
 
 for rec in SeqIO.parse(lspq_sequences,'fasta'):
     seqid = re.search(r'(^\S+)\/\S+\/\S+',rec.id).group(1)
     rec_id_list.append(seqid)
-
+    rec_list.append(rec)
 
 subset_lspq = df_lspq[df_lspq['NO_LSPQ'].isin(rec_id_list)]
 subset_gisaid = df_gisaid[df_gisaid['strain'].isin(rec_id_list)]
@@ -74,3 +89,5 @@ subset_gisaid_subcol.loc[:,'rta_exposure'] = subset_gisaid_subcol.country_exposu
 
 subset_lspq_subcol.to_csv(out_metadata,sep="\t",index=False)
 subset_gisaid_subcol.to_csv(out_metadata,sep="\t",index=False,mode='a',header=False)
+
+SeqIO.write(rec_list,out_sequences,'fasta')
