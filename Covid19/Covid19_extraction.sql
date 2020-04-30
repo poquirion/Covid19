0c1c5945f13c022677e35484d006fb53ad51e3af
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-04-30 10:37:03 -0400

    Eric Fournier 2020-04-30: correction pour les rss null

--- Covid19/Covid19_extraction.sql
+++ Covid19/Covid19_extraction.sql
@@ -1,4 +1,4 @@
-with v as (select NOLSPQ as no_lspq, AGE_ANNEE as age, RSS_PATIENT as rss_patient,AUCUN_VOYAGE as aucun_voyage, VOYAGE_PAYS_1 as voyage_pays_1, DATE_PRELEVEMENT as date_prelev, DATE_RECEPTION as date_recu,
+with v as (select NOLSPQ as no_lspq, AGE_ANNEE as age,SEX as sex, RSS_PATIENT as rss_patient,AUCUN_VOYAGE as aucun_voyage, VOYAGE_PAYS_1 as voyage_pays_1, DATE_PRELEVEMENT as date_prelev, DATE_RECEPTION as date_recu,
 CH as ch,RESULTAT_LABORATOIRE as res_lab, SUBSTR(POSTAL_CODE,1,3) as postal_code ,MAX(nCov2019_nCov_ARN) as max_res from (select distinct f.folderno as NOLSPQ, to_char(cr.BIRTH_DATE,'YYYY-MM-DD') as DATE_NAISSANCE,
 p.LAST_NAME as NOM, p.FIRST_NAME as PRENOM, 
 case 
@@ -7,13 +7,26 @@ case
     else
         TRUNC(MONTHS_BETWEEN(COALESCE(cr.DATE_COLLECTED,cr.DATE_RECEIVED),cr.BIRTH_DATE )/12,2)
 end as AGE_ANNEE,
-p.SEX as SEX, cr.OWNER_COUNTY as RSS_PATIENT, cr.OWNER_ZIP as POSTAL_CODE,
+p.SEX as SEX, 
+ 
+case
+    when cr.OWNER_COUNTY is null
+    then (select REGION_NAME from REGION where TYPE = 'RSS' and REGION_CODE = rc.LSPQ_RSS)
+    else cr.OWNER_COUNTY
+end as RSS_PATIENT,
+
+COALESCE(cr.OWNER_ZIP,rc.ZIP) as POSTAL_CODE,
 case 
     when crm.FIELD45 is null 
     then 'O' 
     else 'N' 
 end AS AUCUN_VOYAGE,
-crm.FIELD45 as VOYAGE_PAYS_1,
+case 
+    when crm.FIELD45 is null
+    then 'AUCUN_VOYAGE'
+    else crm.FIELD45
+end as VOYAGE_PAYS_1,
+
 nvl(to_char(cr.DATE_COLLECTED, 'YYYY-MM-DD'),'') AS DATE_PRELEVEMENT,
 nvl(to_char(cr.DATE_RECEIVED, 'YYYY-MM-DD'),'') AS DATE_RECEPTION,
 rc.COMPNAME AS CH,
@@ -39,9 +52,13 @@ from
 		inner join ORDTASK ot on ot.ORDNO = o.ORDNO
         
 where 
-		ot.TESTCODE in (2685,2689,2167) and cr.DATE_RECEIVED > to_date('2020-04-20', 'YYYY-MM-DD') 
-        and cr.PANEL_LIST like '2019-nCoV%' and rc.RASCLIENTID not in ('LSPQCEC','LSPQCIC','LSPQF','LSPQP','LSPQV')  
+		ot.TESTCODE in (2685,2689,2167) and cr.DATE_RECEIVED > to_date('2020-01-01', 'YYYY-MM-DD') 
+        and cr.PANEL_LIST like '2019-nCoV%' and rc.RASCLIENTID not in ('LSPQCEC','LSPQCIC','LSPQF','LSPQP','LSPQV') 
         
-order by f.FOLDERNO)   GROUP BY NOLSPQ, AGE_ANNEE, RSS_PATIENT,AUCUN_VOYAGE, VOYAGE_PAYS_1, DATE_PRELEVEMENT, DATE_RECEPTION,
-CH,RESULTAT_LABORATOIRE, POSTAL_CODE ORDER BY NOLSPQ) select v.no_lspq,v.age,v.rss_patient,v.aucun_voyage, v.voyage_pays_1, v.date_prelev,
-v.date_recu, v.ch, v.postal_code,  v.max_res from v where  v.max_res in ('DÃ©tectÃ©','dÃ©tectÃ©');   
+order by f.FOLDERNO)   GROUP BY NOLSPQ, AGE_ANNEE,SEX, RSS_PATIENT,AUCUN_VOYAGE, VOYAGE_PAYS_1, DATE_PRELEVEMENT, DATE_RECEPTION,
+CH,RESULTAT_LABORATOIRE, POSTAL_CODE ORDER BY NOLSPQ) select v.no_lspq,v.age,v.sex,v.rss_patient,v.aucun_voyage, v.voyage_pays_1, v.date_prelev,
+v.date_recu, v.ch, v.postal_code,  v.max_res from v where  v.max_res in ('Détecté','détecté');    
+
+
+
+
